3503e63fd3276763d8a7a109183bd6ac02327150
rybydrapiezne <szymik43@gmail.com>
2022-11-15 00:35:23 +0100

    Boulder generation

--- src/Board/Boulder.java
+++ src/Board/Boulder.java
@@ -1,6 +1,13 @@
 package Board;
 
 public class Boulder implements GameObject {
+    int x;
+    int y;
+    Boulder(int x,int y)
+    {
+        this.x=x;
+        this.y=y;
+    }
     @Override
     public int get_position() {
         return 0;
--- src/Board/Plane.java
+++ src/Board/Plane.java
@@ -1,14 +1,18 @@
 package Board;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Plane {
     public static int SIZE=20;
-    public static int LEVEL_LENGHT=5;
+    public static int LEVEL_LENGTH =5;
+    int number_of_boulders;
+
    public ArrayList<ArrayList<GameObject>> board;
-    public Plane()
+    public Plane(Difficulty difficulty)
     {
 
+        set_difficulty(difficulty);
         board=new ArrayList<>();
         for(int i=0;i<SIZE;i++) {
             board.add(new ArrayList<>());
@@ -27,21 +31,22 @@ public class Plane {
             board.get(i).set(0,new Wall(i,0));
         }
         inner_walls();
+        generate_boulders();
     }
 
     public void inner_walls()
     {
         boolean flag=false;
-        for(int i=LEVEL_LENGHT;i< board.size();i+=LEVEL_LENGHT)
+        for(int i = LEVEL_LENGTH; i< board.size(); i+= LEVEL_LENGTH)
         {
             if(!flag) {
-                for (int j = 1; j < board.get(i).size() - 1 - LEVEL_LENGHT; j++) {
+                for (int j = 1; j < board.get(i).size() - 1 - LEVEL_LENGTH; j++) {
                     board.get(i).set(j, new Wall(i, j));
                     flag=true;
                 }
             }
             else {
-                for (int j = board.get(i).size() - 2; j > LEVEL_LENGHT; j--)
+                for (int j = board.get(i).size() - 2; j > LEVEL_LENGTH; j--)
                     board.get(i).set(j, new Wall(i, j));
                     flag=false;
             }
@@ -55,10 +60,38 @@ public class Plane {
             {
                 if(board.get(i).get(j) instanceof Wall)
                     System.out.print("1");
-                else
+                else if(board.get(i).get(j) instanceof Dirt)
                     System.out.print("0");
+                else if(board.get(i).get(j) instanceof Boulder)
+                    System.out.print("2");
             }
             System.out.println();
+        }
+    }
+    public void generate_boulders()
+    {
+        Random random=new Random();
+        int x;
+        int y;
+        for(int i=0;i<number_of_boulders;)
+        {
+            x= random.nextInt(1,board.size()-1);
+            y=random.nextInt(1,board.size()-1);
+            if(!(board.get(x).get(y) instanceof Wall))
+            {
+                i++;
+                board.get(x).set(y,new Boulder(x,y));
+            }
+        }
+    }
+    public void set_difficulty(Difficulty difficulty)
+    {
+        switch (difficulty)
+        {
+            case Easy -> number_of_boulders=50;
+            case Medium -> number_of_boulders=75;
+            case Hard -> number_of_boulders=100;
+
         }
     }
 }
