4da3c25a7500a32f1e7bf5e01888149e0f76e275
rybydrapiezne <szymik43@gmail.com>
2022-11-15 00:07:59 +0100

    Inner walls and board printing as text method

--- src/Board/Plane.java
+++ src/Board/Plane.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Plane {
     public static int SIZE=20;
+    public static int LEVEL_LENGHT=5;
    public ArrayList<ArrayList<GameObject>> board;
     public Plane()
     {
@@ -25,7 +26,39 @@ public class Plane {
             board.get(board.size()-1).set(i,new Wall(board.size()-1, i));
             board.get(i).set(0,new Wall(i,0));
         }
-
+        inner_walls();
     }
 
+    public void inner_walls()
+    {
+        boolean flag=false;
+        for(int i=LEVEL_LENGHT;i< board.size();i+=LEVEL_LENGHT)
+        {
+            if(!flag) {
+                for (int j = 1; j < board.get(i).size() - 1 - LEVEL_LENGHT; j++) {
+                    board.get(i).set(j, new Wall(i, j));
+                    flag=true;
+                }
+            }
+            else {
+                for (int j = board.get(i).size() - 2; j > LEVEL_LENGHT; j--)
+                    board.get(i).set(j, new Wall(i, j));
+                    flag=false;
+            }
+        }
+    }
+    public void print_text_board()
+    {
+        for(int i=0;i<board.size();i++)
+        {
+            for(int j=0;j<board.get(i).size();j++)
+            {
+                if(board.get(i).get(j) instanceof Wall)
+                    System.out.print("1");
+                else
+                    System.out.print("0");
+            }
+            System.out.println();
+        }
+    }
 }
